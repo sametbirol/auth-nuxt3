28d9f4885b83b6edbc5b5fb9ab11252b2fd548b5
Samet Birol <sametbirol34@gmail.com>
2023-12-22 13:10:04 +0300

    About page will hold user info

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        About Page
+        <div v-if="firebaseUser">{{ firebaseUser }}</div>
+        <div v-else>You are not logged in!</div>
     </div>
 </template>
 
 <script setup>
-
+import { storeToRefs } from 'pinia'; // import storeToRefs helper hook from pinia
+import { useAuthStore } from '~/store/auth';
+const { firebaseUser } = storeToRefs(useAuthStore());
 </script>
 
 <style scoped></style>
