64d43dabdc1f65732a1fdae8ba82dd6716fee77e
Samet Birol <sametbirol34@gmail.com>
2023-12-22 14:49:02 +0300

    navigation  guard

---
+++ middleware/auth.ts
@@ -0,0 +1,7 @@
+import { useAuthStore } from "~/store/auth"
+const {isAuthenticated} = storeToRefs(useAuthStore())
+export default defineNuxtRouteMiddleware((to, from) => {
+    if(!isAuthenticated.value){
+        return navigateTo('/login')
+    }
+})
