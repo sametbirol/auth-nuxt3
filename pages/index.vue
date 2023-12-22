d1e89f47dc92dc64d4fe5da462bd899030cfbcef
Samet Birol <sametbirol34@gmail.com>
2023-12-22 13:10:29 +0300

    home page empty  / will show products here

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
     <div>
-        <div v-if="firebaseUser">{{ firebaseUser }}</div>
-        <div v-else>You are not logged in!</div>
+        
     </div>
 </template>
 
 <script setup>
-import { storeToRefs } from 'pinia'; // import storeToRefs helper hook from pinia
-import { useAuthStore } from '~/store/auth';
-const { firebaseUser } = storeToRefs(useAuthStore());
+
 
 </script>
 
