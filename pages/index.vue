665e48f0c999d42071d508a22489839582866adb
Samet Birol <sametbirol34@gmail.com>
2023-12-28 21:07:29 +0300

    product fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        
+    <div class="flex gap-4 p-4 flex-wrap justify-between" >
+        <CardProduct v-for="product in products.products" :product="product"> </CardProduct>
     </div>
 </template>
 
 <script setup>
-
+import { useAuthStore } from '~/store/auth';
+const { initUser } = useAuthStore()
+onMounted(() => {
+    initUser();
+})
+const { data: products } = await useFetch('https://dummyjson.com/products')
 
 </script>
 
