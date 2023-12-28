efc6527f2ad9c1b48c92bf28c7d7ffcafb96476f
Samet Birol <sametbirol34@gmail.com>
2023-12-29 00:05:15 +0300

    product page moved from home page

---
+++ pages/product/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex gap-8 p-16 flex-wrap justify-center bg-gray-400 h-full">
+        <CardProduct v-for="product in products" :product="product"> </CardProduct>
+    </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useProductsStore } from '~/store/products';
+const { getProducts } = useProductsStore()
+const { products } = storeToRefs(useProductsStore())
+onMounted(async () => {
+    await getProducts();
+})
+</script>
+
+<style scoped></style>
