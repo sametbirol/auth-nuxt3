e74accd72997d534d81efaac16c31db6b7e613bf
Samet Birol <sametbirol34@gmail.com>
2023-12-28 23:59:19 +0300

    redirect to single product page

--- components/Card/Product.vue
+++ components/Card/Product.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class=" flex flex-col rounded-xl bg-gray-100 w-48 hover:scale-110 overflow-hidden shadow ">
+    <NuxtLink :to="`/product/${id}`" class="flex flex-col rounded-xl bg-gray-100 w-48 hover:scale-110 overflow-hidden shadow ">
         <img :src="images[images.length - 1]" class="w-full h-48">
         <p class="w-full text-center p-1 font-bold">{{ title }}</p>
         <p class="w-full p-2 ellipsis">{{ description }}</p>
         <CardRating class="align-right" :rating="rating" :limit="5" :id="id"></CardRating>
-    </div>
+    </NuxtLink>
 </template>
 
 <script setup>
