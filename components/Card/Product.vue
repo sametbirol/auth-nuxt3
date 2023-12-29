a6675d4baeda6a00918c2bf3b0387705fe6837d3
Samet Birol <sametbirol34@gmail.com>
2023-12-29 16:27:33 +0300

    add price and discount

--- components/Card/Product.vue
+++ components/Card/Product.vue
@@ -1,17 +1,28 @@
 <template>
-    <NuxtLink :to="`/product/${id}`" class="flex flex-col rounded-xl bg-gray-100 w-48 hover:scale-110 overflow-hidden shadow ">
-        <img :src="images[images.length - 1]" class="w-full h-48">
-        <p class="w-full text-center p-1 font-bold">{{ title }}</p>
-        <p class="w-full p-2 ellipsis">{{ description }}</p>
-        <CardRating class="align-right" :rating="rating" :limit="5" :id="id"></CardRating>
+    <NuxtLink :to="`/product/${id}`"
+        class="flex flex-col rounded-xl bg-gray-100 w-48 hover:scale-110 overflow-hidden shadow ">
+        <img :src="thumbnail" class="h-48 object-cover">
+        <p class="w-full text-center p-1 font-bold ellipsis">{{ title }}</p>
+        <p class="w-full p-2 text-sm overflow-hidden ellipsis">{{ description }}</p>
+        <p class="w-full text-center p-1 font-semibold ">${{ price }}
+            <span class="text-sm">{{ discountPercentage }}% off</span>
+        </p>
+        <CardRating class="self-center" :rating="rating" :limit="5" :id="id"></CardRating>
+
     </NuxtLink>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { type PropType } from 'vue'
+import type { Product } from '~/store/products'
 const props = defineProps({
-    product: Object,
+    product: {
+        type: Object as PropType<Product>,
+        required: true,
+    }
 })
-const { id, title, description, price, rating, images } = props.product
+
+const { id, title, description, price, rating, thumbnail, discountPercentage } = props.product
 </script>
 
 <style scoped>
