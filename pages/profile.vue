1d83889c34c68dee02bd241783ab18c261315f0a
Samet Birol <sametbirol34@gmail.com>
2023-12-29 16:28:18 +0300

    update photo funcionalty improved

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="container mx-auto px-4 py-8">
             <div class="flex items-center flex-col justify-center mb-8">
-                <img :src="user.photoURL" alt="Profile Picture" class="w-48 h-48 rounded-full">
+                <img :src="user.photoURL" alt="Profile Picture" class="w-48 h-48 rounded-full object-cover">
                 <button @click="showModal = true" class="px-2 py-1 bg-blue-500 text-white rounded-md">Edit</button>
                 <div v-if="showModal"
                     class="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
@@ -10,29 +10,29 @@
                         <button @click="showModal = false"
                             class="absolute top-4 right-4 px-2 py-1 bg-red-500 text-white rounded-md">Close</button>
                         <div class="flex flex-col items-center">
-                            <img :src="previewImage" class="w-48 h-48 rounded-full" />
+                            <img :src="previewImage || user.photoURL" class="w-48 h-48 rounded-full object-cover" />
                             <input type="file" ref="photoUpload" accept="image/png, image/jpeg" class="mb-4"
                                 @change="handleFileChange">
-                            <button @click.prevent="handleUpdate"
-                                class="px-4 py-2 bg-blue-500 text-white rounded-md">Update</button>
+                            <button @click.prevent="handleUpdate" class="px-4 py-2 bg-blue-500 disabled:bg-blue-200 text-white rounded-md"
+                                :disabled="!previewImage">Update</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-1 gap-4 self-center">
                 <div>
                     <h2 class="text-xl font-semibold mb-2">Profile Information</h2>
                     <div class="bg-white rounded-lg p-4 shadow">
                         <p><span class="font-semibold">Email:</span> {{ user.email }}</p>
                         <p><span class="font-semibold">Email Verified:</span> {{ user.emailVerified ? 'Yes' : 'No' }}
                         </p>
-                        <p><span class="font-semibold">Provider:</span> {{ user.providerData[0].providerId }}</p>
+                        <p><span class="font-semibold">Authentication Method:</span> {{ user.providerData[0].providerId }}
+                        </p>
                         <p><span class="font-semibold">Anonymous:</span> {{ user.isAnonymous ? 'Yes' : 'No' }}</p>
                         <p><span class="font-semibold">UID:</span> {{ user.uid }}</p>
-                        <p><span class="font-semibold">Created At:</span> {{ formatDate(user.createdAt) }}</p>
-                        <p><span class="font-semibold">Last Login At:</span> {{ formatDate(user.lastLoginAt) }}</p>
+                        <p><span class="font-semibold">Created At:</span> {{ formatDate(user.metadata.createdAt) }}</p>
+                        <p><span class="font-semibold">Last Login At:</span> {{ formatDate(user.metadata.lastLoginAt) }}</p>
                     </div>
-
                 </div>
                 <!-- Other user-related information or actions -->
             </div>
@@ -47,9 +47,10 @@ definePageMeta({
     middleware: 'auth',
 })
 const formatDate = (timestamp) => {
-    return new Date(parseInt(timestamp)).toLocaleString();
+    // return new Date(parseInt(timestamp)).toLocaleString();
+    return timestamp
 };
-import { storeToRefs } from 'pinia'; 
+import { storeToRefs } from 'pinia';
 import { useAuthStore } from '~/store/auth';
 const { user } = storeToRefs(useAuthStore());
 const { uploadPhoto } = usePhotoFirebase();
@@ -66,10 +67,21 @@ const handleFileChange = () => {
         };
         reader.readAsDataURL(file);
     }
+    else{
+        previewImage.value = null;
+    }
 };
 const handleUpdate = () => {
-    uploadPhoto(photoUpload.value.files[0], user.value.providerData[0].email);
+    uploadPhoto(photoUpload.value.files[0], user.value.uid);
 }
+
+// import { useProductsStore } from '~/store/products';
+const { initUser } = useAuthStore()
+// const { getProducts } = useProductsStore()
+onBeforeMount(async () => {
+    initUser();
+    // await getProducts();
+})
 </script>
 
 <style scoped>
