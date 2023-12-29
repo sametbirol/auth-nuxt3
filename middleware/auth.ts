0bfeed8d48bdec31737ab40a627aa1e3f7b63aae
Samet Birol <sametbirol34@gmail.com>
2023-12-29 16:28:02 +0300

    auth middleware sends to home

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,6 +2,7 @@ import { useAuthStore } from "~/store/auth"
 const { isAuthenticated } = storeToRefs(useAuthStore())
 export default defineNuxtRouteMiddleware((to, from) => {
     if (!isAuthenticated.value) {
-        return navigateTo('/login')
+        console.log("sending to login page")
+        return navigateTo('/')
     }
 })
